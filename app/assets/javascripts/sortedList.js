f1c9923379b75b56917793b68a5d0669150aa786
rachel hazes <rahazes@gmail.com>
2015-03-19 14:21:24 -0400

    added non prroroty to current_list.html.erb

--- app/assets/javascripts/sortedList.js
+++ app/assets/javascripts/sortedList.js
@@ -1,11 +1,10 @@
 console.log('we are connected to sortedList.js');
 var collapsableList = function() {
-  $('.clicker-div').hide();
+  // $('.clicker-div').hide();
   var actionDivs = $('.action-div');
   for (i=0; i < actionDivs.length; i++) {
       actionDivs[i].addEventListener('click', function(evt) {
           $(this).children('.clicker-div').slideToggle("slow")
        });
     }
-
 }
