75643aa86c4dda958a0ca5f1c1466bf6468ce4d1
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-03-19 14:46:13 -0400

    changes from a click to mouseenter for the current list page

--- app/assets/javascripts/sortedList.js
+++ app/assets/javascripts/sortedList.js
@@ -3,7 +3,10 @@ var collapsableList = function() {
   // $('.clicker-div').hide();
   var actionDivs = $('.action-div');
   for (i=0; i < actionDivs.length; i++) {
-      actionDivs[i].addEventListener('click', function(evt) {
+      actionDivs[i].addEventListener('mouseenter', function(evt) {
+          $(this).children('.clicker-div').slideToggle("slow")
+       });
+      actionDivs[i].addEventListener('mouseleave', function(evt) {
           $(this).children('.clicker-div').slideToggle("slow")
        });
     }
