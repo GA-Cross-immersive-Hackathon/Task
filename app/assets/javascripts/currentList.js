07f7640df55b7784aef4f01f48a40a3ff91849ce
akosua kernizan <akernizan@gmail.com>
2015-03-20 21:16:34 -0400

    aded style to buttons and login/signup page
    added function for progressbar

--- app/assets/javascripts/currentList.js
+++ app/assets/javascripts/currentList.js
@@ -10,3 +10,52 @@ var collapsableList = function() {
        });
     }
 }
+
+
+console.log('test is connected')
+
+
+$(document).on("ready", function() {
+	var  counter= 0;
+	function progressBar(){
+		var colors = ["#96F2FC", "#00DDFF", "#00B4FF", "#2366D0"];
+
+				var $newDiv = $('<div>');
+				$newDiv.width(100);
+				$newDiv.height(100);
+				$('#hello').append($newDiv);	
+		if (counter < colors.length) {
+			$newDiv.css({
+				'background-color': colors[counter],
+				'border-radius': 5,
+				'margin-left': 10,
+				'float': 'left'
+		});
+			counter++;	
+		} else {
+			counter = 0;
+			$('#hello').children().remove();
+		}
+}
+	
+
+function render(){
+	setInterval(function(){
+		progressBar();
+	},1000)
+}
+
+render();
+
+});
+
+
+
+
+
+
+
+
+
+
+
