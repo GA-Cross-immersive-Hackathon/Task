0a76794e8ef8fbf8d5225c401670b396935c93ae
rachel hazes <rahazes@gmail.com>
2015-03-21 16:41:59 -0400

    WIP current_list modal

--- app/assets/javascripts/currentList.js
+++ app/assets/javascripts/currentList.js
@@ -1,4 +1,6 @@
 console.log('we are connected to currentList.js');
+
+// function called on current_list.html.erb to enable collapse feature
 var collapsableList = function() {
   var actionDivs = $('.action-div');
   for (i=0; i < actionDivs.length; i++) {
@@ -11,54 +13,36 @@ var collapsableList = function() {
     }
 }
 
+// progress bar to be displayed while user is accomplishing task
+var  counter= 0;
+var progressBar = function() {
+  var colors = ["#96F2FC", "#00DDFF", "#00B4FF", "#2366D0"];
 
-console.log('test is connected')
+      var $newDiv = $('<div>');
+      $newDiv.width(25);
+      $newDiv.height(25);
+      $('#progressBar').append($newDiv);
+  if (counter < colors.length) {
+    $newDiv.css({
+      'background-color': colors[counter],
+      'border-radius': 5,
+      'margin-left': 10,
+      'float': 'left'
+  });
+    counter++;
+  } else {
+    counter = 0;
+    $('#hello').children().remove();
+  }
 
-
-$(document).on("ready", function() {
-	var  counter= 0;
-	function progressBar(){
-		var colors = ["#96F2FC", "#00DDFF", "#00B4FF", "#2366D0"];
-
-				var $newDiv = $('<div>');
-				$newDiv.width(100);
-				$newDiv.height(100);
-				$('#hello').append($newDiv);	
-		if (counter < colors.length) {
-			$newDiv.css({
-				'background-color': colors[counter],
-				'border-radius': 5,
-				'margin-left': 10,
-				'float': 'left'
-		});
-			counter++;	
-		} else {
-			counter = 0;
-			$('#hello').children().remove();
-		}
-}
-	
-
-function render(){
-	setInterval(function(){
-		progressBar();
-	},1000)
 }
 
-render();
+function renderProgressBar(){
+	setInterval(progressBar(),1000)
+}
+renderProgressBar();
 
-});
-
-
-
-
-
-
-
-
-
-
-
+// function to call the
 var startTimer = function(e) {
   var xhr = new XMLHttpRequest();
       xhr.open('GET', '/token.json');
@@ -115,10 +99,37 @@ var stopTimer = function(e) {
 }
 
 
+var dynamicModal = function() {
+  console.log("we're inside the dynamical modal");
+  $('#choosingTaskModal').on('show.bs.modal', function (event) {
+
+    e.preventDefault();
+    console.log("default should have been prevented");
+    // var h3 = $(this).parent().parent().children('.action')
+    // var action = (h3.text());
+    var button = $(event.relatedTarget)
+    console.log(button)
+    console.log(button.data) // Button that triggered the modal
+    var task = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('You have started task:' + task)
+    // modal.find('.modal-body input').val(recipient)
+  })
+
+  grabStopButtons();
+  $('#choosingTaskModal').modal('show');
+
+}
+
+
+
 var grabStartButtons = function() {
 
   $('.doButton').on('click', function(e) {
     startTimer(e);
+    dynamicModal();
     // var h3 = $(this).parent().parent().children('.action')
     // var action = (h3.text());
     //  make an ajax call for get token, then ajax to update the server. the object being sent should
@@ -131,21 +142,3 @@ var grabStopButtons = function() {
     stopTimer(e);
 });
 }
-// var dynamicModal = function() {
-//
-//   $('#choosingTaskModal').on('show.bs.modal', function (event) {
-//     // var h3 = $(this).parent().parent().children('.action')
-//     // var action = (h3.text());
-//     var button = $(event.relatedTarget)
-//     console.log(button)
-//     console.log(button.data) // Button that triggered the modal
-//     var task = button.data('whatever') // Extract info from data-* attributes
-//     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//     var modal = $(this)
-//     modal.find('.modal-title').text('You have started task:' + task)
-//     // modal.find('.modal-body input').val(recipient)
-//   })
-//
-// }
->>>>>>> master
